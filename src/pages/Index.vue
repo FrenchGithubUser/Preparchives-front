bff651cdf00ec6b0971bd73ede317606ad617823
ThomasTraineau <thomas_traineau@tutanota.com>
2022-09-27 17:41:44 +0200

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="index-container">
+    <div class="title">PrepArchives</div>
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <q-btn no-caps label="Chercher un sujet/corrigé" class="browse-button"/>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+<style lang="scss" scoped>
+.index-container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .title{
+    font-size: 2em;
+    margin-bottom: 50px;
+    margin-top: 50px;
+  }
+  .browse-button{
+    margin-top: 50px;
+  }
+}
+</style>
